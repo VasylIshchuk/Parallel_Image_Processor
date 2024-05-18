3b24ca74b1ea0cfee6a413725851305ff19a34e4
VasylIshchuk <Vasyl.Ishchuk@live.umcs.edu.pl>
2024-05-18 14:06:54 +0200

    Implement multithreaded image channel histogram calculation using thread pool

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Locale;
 
 public class Main {
@@ -6,15 +7,15 @@ public class Main {
         ip.loadImage("image.png");
 
         long startTime1 = System.currentTimeMillis();
-        ip.increaseBrightness(0);
+        List<Integer> listRed = ip.getImageHistogramChannel("red");
         long stopTime1 = System.currentTimeMillis();
 
         long startTime2 = System.currentTimeMillis();
-        ip.increaseBrightnessWithThreads(0);
+         List<Integer> listBlue = ip.getImageHistogramChannel("blue");
         long stopTime2 = System.currentTimeMillis();
 
         long startTime3 = System.currentTimeMillis();
-        ip.increaseBrightnessWithPoolOfThreads(100);
+        List<Integer> listGreen = ip.getImageHistogramChannel("green");
         long stopTime3 = System.currentTimeMillis();
 
         System.out.println(String.format(Locale.ENGLISH,
@@ -24,6 +25,6 @@ public class Main {
         System.out.println(String.format(Locale.ENGLISH,
                 "With pool of threads: %d",stopTime3 - startTime3));
 
-        ip.saveImage("brighter_image.png", "png");
+//        ip.saveImage("brighter_image.png", "png");
     }
 }
